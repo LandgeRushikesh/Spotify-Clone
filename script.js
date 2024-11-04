f535879104a16bb05d4309fe2358eea7259de672
LandgeRushikesh <rushilandge12@gmail.com>
2024-11-04 23:31:20 +0530

    Bugs fixed in play/pause feature

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 let currentSong = new Audio()//to play one song at a time
+let pausedAt = 0;//for playing the song from where it is paused
+let isPaused = false //for checking whether the song is paused or not 
 
 // convert seconds to minutes for song time
 function convertSecondsToMinutes(seconds) {
@@ -58,38 +60,36 @@ async function main() {
 
     list.forEach(element =>{
         element.addEventListener("click",()=>{
-            playMusic(element.querySelector(".info").textContent)
+            if(element.querySelector(".play-btn").classList.contains("play")){
+                element.querySelector(".play-btn").innerHTML = '<i class="fa-solid fa-pause"></i>'
+                element.querySelector(".play-btn").classList.remove("play")
+                element.querySelector(".play-btn").classList.add("pause")
+                playBtn.innerHTML = '<i class="fa-solid fa-pause"></i>'
+                playMusic(element.querySelector(".info").textContent,isPaused,pausedAt)
+                
+            }
+            else if(element.querySelector(".play-btn").classList.contains("pause")){
+                element.querySelector(".play-btn").innerHTML = '<i class="fa-solid fa-play "></i>'
+                element.querySelector(".play-btn").classList.remove("pause")
+                element.querySelector(".play-btn").classList.add("play")
+                playBtn.innerHTML = '<i class="fa-solid fa-play "></i>'
+                pausedAt = currentSong.currentTime
+                isPaused = true
+                currentSong.pause();
+            } 
         })
     })
 } 
 
-let playMusic = (track)=>{
+let playMusic = (track,isPaused,pausedAt)=>{
     // In JavaScript, the Audio.src property is used to set or get the source (URL) of the audio file that you want to play using the Audio object.
     currentSong.src = "http://127.0.0.1:3000/spotify_clone/songs/"+decodeURI(track)
-    
+    if(isPaused){
+        currentSong.currentTime = pausedAt
+    }
+    currentSong.play();
     document.body.querySelector(".songInfo").innerText = track;
-    document.body.querySelector(".songTime").innerText = "00.00/00.00";
-    let playBtn = document.querySelector(".songButtons").querySelector(".play-btn")
-    // update play-pause button
-    document.querySelectorAll(".play").forEach(ele=>{
-        // console.log(ele)
-        ele.addEventListener("click",()=>{
-            if(ele.querySelector(".play-btn").classList.contains("play")){
-                ele.querySelector(".play-btn").innerHTML = '<i class="fa-solid fa-pause"></i>'
-                ele.querySelector(".play-btn").classList.remove("play")
-                ele.querySelector(".play-btn").classList.add("pause")
-                playBtn.innerHTML = '<i class="fa-solid fa-pause"></i>'
-                currentSong.play();
-            }
-            else if(ele.querySelector(".play-btn").classList.contains("pause")){
-                ele.querySelector(".play-btn").innerHTML = '<i class="fa-solid fa-play "></i>'
-                ele.querySelector(".play-btn").classList.remove("pause")
-                ele.querySelector(".play-btn").classList.add("play")
-                playBtn.innerHTML = '<i class="fa-solid fa-play "></i>'
-                currentSong.pause();
-            }   
-        })
-    })    
+    document.body.querySelector(".songTime").innerText = "00.00/00.00";   
 }
 
 // get song time update
